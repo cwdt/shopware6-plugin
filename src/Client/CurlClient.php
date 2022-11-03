e4a5be5446d234754cca36d8ca238392f3d74145
Marco Veenendaal <marco@picqer.com>
2022-11-03 12:42:33 +0100

    Adding user-agent

--- src/Client/CurlClient.php
+++ src/Client/CurlClient.php
@@ -4,6 +4,8 @@ namespace Picqer\Shopware6Plugin\Client;
 
 final class CurlClient implements Client
 {
+    const USER_AGENT = 'Picqer Shopware 6 Plugin (version 0.1.0)';
+
     public function pushOrder(string $subdomain, string $connectionKey, string $id): void
     {
         $this->post(
@@ -14,18 +16,20 @@ final class CurlClient implements Client
 
     private function post(string $url, array $body): void
     {
-        $curlSession = curl_init();
+        $session = curl_init();
 
-        curl_setopt($curlSession, CURLOPT_URL, $url);
-        curl_setopt($curlSession, CURLOPT_HTTPHEADER, [
+        curl_setopt($session, CURLOPT_USERAGENT, self::USER_AGENT);
+
+        curl_setopt($session, CURLOPT_URL, $url);
+        curl_setopt($session, CURLOPT_HTTPHEADER, [
             'Content-Type: application/json',
             'Accept: application/json'
         ]);
 
-        curl_setopt($curlSession, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($curlSession, CURLOPT_POSTFIELDS, json_encode($body));
+        curl_setopt($session, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($session, CURLOPT_POSTFIELDS, json_encode($body));
 
-        curl_exec($curlSession);
-        curl_close($curlSession);
+        curl_exec($session);
+        curl_close($session);
     }
 }
